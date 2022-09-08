4b3c002c279432bcaeea07ebfe872ff1c1f24fde
Staick <staick@163.com>
2022-09-08 20:01:21 +0800

    Finish SMS veritification code.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from tkinter import Image
 from django.urls import path
-from users.views import RegisterView, ImageCodeView
+from users.views import RegisterView, ImageCodeView, SmsCodeView
 
 urlpatterns = [
     # 第一个参数：路由
@@ -8,5 +8,8 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 
     # 图片验证码的路由
-    path('imagecode/', ImageCodeView.as_view(), name='imagecode')
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
+
+    # 短信验证码的路由
+    path('smscode/', SmsCodeView.as_view(), name='smscode'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
+import logging
+from random import randint
 from django.shortcuts import render
 from django.views import View
-from django.http.response import HttpResponseBadRequest, HttpResponse
+from django.http.response import HttpResponseBadRequest, HttpResponse, JsonResponse
 from django_redis import get_redis_connection
 from libs.captcha.captcha import captcha
+from libs.yuntongxun.sms import CCP
+from utils.response_code import RETCODE
+
+logger = logging.getLogger('django')
 
 class RegisterView(View):
     def get(self, request):
@@ -26,3 +32,41 @@ class ImageCodeView(View):
         # 5.返回图片二进制
         return HttpResponse(image, content_type='image/jpeg')
 
+
+class SmsCodeView(View):
+
+    def get(self, request):
+
+        # 1.接收参数（查询字符串）
+        mobile = request.GET.get('mobile')
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        # 2.参数的验证
+        # 2.1 验证参数是否齐全
+        if not all([mobile, image_code, uuid]):
+            return JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必要的参数'})
+        # 2.2 图片验证码的验证
+        # 连接redis，获取redis中的图片验证码
+        redis_conn = get_redis_connection('default')
+        redis_image_code = redis_conn.get(f'img:{uuid}')
+        # 判断图片验证码是否存在
+        if redis_image_code is None:
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码已过期'})
+        # 如果图片验证码没有过期，获取之后，删除验证码
+        try:
+            redis_conn.delete(f'img:{uuid}')
+        except Exception as e:
+            logger.error(e)
+        # 对比图片验证码，注意大小写，redis的数据是byte类型
+        if redis_image_code.decode().lower() != image_code.lower():
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码错误'})
+        # 3.生成短信验证码
+        sms_code = f'{randint(0, 999999):06d}'
+        # 为了后期比对方便，可以将短信验证码，记录到日志中
+        logger.info(sms_code)
+        # 4.保存短信验证码到redis中
+        redis_conn.setex(f'sms:{mobile}', 300, sms_code)
+        # 5.发送短信
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        # 6.返回响应
+        return JsonResponse({'code': RETCODE.OK, 'msg':'短信发送成功'})
