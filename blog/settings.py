65cca403b1175d82bc7dc9acfa7ef55344c7266c
Staick <staick@163.com>
2022-09-08 15:56:46 +0800

    finish database configuration, and use mysql.

--- blog/settings.py
+++ blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # Database engine
+        'HOST': '127.0.0.1',    # Datebase host
+        'PORT': 3306,
+        'USER': 'itheima',
+        'PASSWORD': '123456',
+        'NAME': 'itheima_blog',
     }
 }
 
